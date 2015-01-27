194e3854a9a10a3175d121f856856eb08feb572b
matteomanzo <imatteo.manzo@gmail.com>
2015-01-27 16:24:49 +0000

    committing to merge

--- server.rb
+++ server.rb
@@ -1,9 +1,21 @@
 require 'sinatra/base'
 require './lib/app.rb'
+require 'data_mapper'
 
 class FiveASide < Sinatra::Base
+
+  env = ENV['RACK_ENV'] || 'development'
+
+  DataMapper.setup(:default, "postgres://localhost/five_a_side_#{env}")
+
+  # require the file that we want to store in the database
+
+  DataMapper.finalize
+
+  DataMapper.auto_upgrade!
+
   get '/' do
-    
+
   end
 
   # start the server if ruby file executed directly
