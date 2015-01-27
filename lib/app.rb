73491695739a350602546ef9456bd22be8a13e19
matteomanzo <imatteo.manzo@gmail.com>
2015-01-27 14:50:39 +0000

    shuffle players and generates two teams

--- lib/app.rb
+++ lib/app.rb
@@ -1,23 +1,34 @@
 class App
 
-  attr_accessor :players
+  attr_accessor :players, :team_one, :team_two
 
   def initialize
     @players = []
-  end
-
-  def teams
-    2
+    @team_one = []
+    @team_two = []
   end
 
   def add_player(name)
     if @players.length >= 12
       raise "Too many players"
-    elsif players.include?(name)
+    elsif @players.include?(name)
       raise "#{name} is already in the system!"
     else
       @players << name
     end
+  end
+
+  def generate_teams(number)
+    players.shuffle!
+    first_five = players[0..(number.to_i/2 -1)]
+    first_five.each{|player| @team_one << player}
+    second_five = players[(number.to_i/2)..number]
+    second_five.each{|player| @team_two << player}
+  end
+
+  def reset_teams
+    @team_one.clear
+    @team_two.clear
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -13,27 +13,41 @@ describe App do
   end
 
   it 'should have two teams' do
-    expect(app.teams).to eq 2
+    expect(app.team_one).to eq []
+    expect(app.team_two).to eq []
   end
 
   it 'should have an array of players' do
     expect(app.players).to eq []
   end
 
-  it 'should be able to add players' do
-    app.add_player('ronaldo')
-    p @players.inspect
-    expect(app.players[0]).to eq('ronaldo')
+  context 'players' do
+
+    it 'should be able to add players' do
+      app.add_player('ronaldo')
+      p @players.inspect
+      expect(app.players[0]).to eq('ronaldo')
+    end
+
+    it 'should not be able to add more than 12 players' do
+      add_twelve_players
+      expect(lambda { app.add_player('pirlo') }).to raise_error(RuntimeError, "Too many players")
+    end
+
+    it 'cannot add the same player twice' do
+      app.add_player('messi')
+      expect(lambda { app.add_player('messi') }).to raise_error(RuntimeError, "messi is already in the system!")
+    end
   end
 
-  it 'should not be able to add more than 12 players' do
-    add_twelve_players
-    expect(lambda { app.add_player('pirlo') }).to raise_error(RuntimeError, "Too many players")
-  end
+  context 'teams' do
 
-  it 'cannot add the same player twice' do
-    app.add_player('messi')
-    expect(lambda { app.add_player('messi') }).to raise_error(RuntimeError, "messi is already in the system!")
+    it 'should create two teams with random players' do
+      add_twelve_players
+      app.generate_teams(12)
+      expect(app.team_one.length).to eq (6)
+      expect(app.team_two.length).to eq (6)
+    end
   end
 
 end
