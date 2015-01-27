b1f16ada0a4be046020bfcfdf4ab797c72472556
guspowell <augustuspowell@me.com>
2015-01-27 12:46:11 +0000

    can add players and can't add more than 12

--- lib/app.rb
+++ lib/app.rb
@@ -1,10 +1,23 @@
 class App
 
+  def initialize
+    @players = []
+  end
+
   def teams
     2
   end
 
   def players
-    []
+    @players
   end
+
+  def add_player(name)
+    if @players.length >= 12
+      raise "Too many players"
+    else
+      @players << name
+    end
+  end
+
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,4 +12,15 @@ describe App do
     expect(app.players).to eq []
   end
 
+  it 'should be able add players' do
+    app.add_player('ronaldo')
+    p @players.inspect
+    expect(app.players[0]).to eq('ronaldo')
+  end
+
+  it 'should not be able to add more than 12 players' do
+    12.times { app.add_player('rooney') }
+    expect(lambda { app.add_player('pirlo') }).to raise_error(RuntimeError, "Too many players")
+  end
+
 end
