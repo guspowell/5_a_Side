80ba7114d5db6c93b51131e9f04ecf70b1e94c55
guspowell <augustuspowell@me.com>
2015-01-28 09:35:43 +0000

    second test writtten

--- spec/features/registering_players_spec.rb
+++ spec/features/registering_players_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 feature "User wants to register" do
 
-  scenario "When being a new player visiting the website" do
+  scenario "visiting the website" do
     visit '/'
     expect(page).to have_content("Not on the system? Enter a username")
   end
+
+  # scenario "being a new player and registering" do
+  #   visit '/'
+  #   fill_in :username, :with => username
+  #   click_button 'submit-register'
+  # end
 
 end
