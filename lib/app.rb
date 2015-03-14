168fcfd0f899f4959601b06fbb3bcbebe8d9f779
matteomanzo <imatteo.manzo@gmail.com>
2015-03-14 10:12:45 +0000

    cleaned css file

--- lib/app.rb
+++ lib/app.rb
@@ -24,9 +24,9 @@ class App
   def generate_teams(number = available_players.length)
     available_players_dup = @available_players.dup.shuffle!
       first_team = available_players_dup[0..(number/2 -1)]
-      first_team.each{|player| @team_one << player if !@team_one.include?(player) && !@team_two.include?(player)}
+      first_team.each{|player| @team_one << player if !@team_one.include?(player) && !@team_two.include?(player)} && @team_two.length >= @team_one.length
       second_team = available_players_dup[(number/2)..-1]
-      second_team.each{|player| @team_two << player if !@team_one.include?(player) && !@team_two.include?(player)}
+      second_team.each{|player| @team_two << player if !@team_one.include?(player) && !@team_two.include?(player)} && @team_one.length >= @team_two.length
   end
 
   def reset_teams
